3d8911a020bcd72247b6c54ac81fd5c18fd3c68e
Hyuk Kim <bbindae@hotmail.com>
2016-01-25 18:48:51 -0800

    second commit

---
+++ src/ContosoBooks/Models/SampleData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContosoBooks.Models
+{
+    public class SampleData
+    {
+        public static void Initializie(IServiceProvider serviceProvider)
+        {
+             
+        }
+    }
+}
